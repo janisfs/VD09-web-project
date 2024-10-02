dfe8b3eff3817012a6d67247e896a9175b1d99f4
janisfs <janisfs@mail.ru>
2024-10-02 22:04:31 +0300

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,54 @@
-def main90:
-    pass
+from flask import render_template, flash, redirect, url_for, request, session
+from app import app, db, bcrypt
+from app.models import User
+from app.forms import RegistrationForm, LoginForm
+from flask_login import login_user, current_user, logout_user, login_required, fresh_login_required
+
+@app.route('/')
+@login_required
+def index():
+    if current_user.is_authenticated:
+        return render_template('index.html', username=current_user.username)
 
 
-if __main__ ='__main__':
-    main()
+app.route('/register', methods=['GET', 'POST'])
+def registration():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Ваш аккаунт был создан. Теперь вы можете войти в него.', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            return redirect(url_for('index'))
+        else:
+            flash('Неверный логин или пароль. Пожалуйста, попробуйте снова.', 'danger')
+    return render_template('login.html', title='Войти', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+@app.route('/click', methods=['POST'])
+@login_required
+def click():
+    if current_user.is_authenticated:
+        current_user.clicks += 1
+        db.session.commit()
+        return redirect(url_for('index'))
