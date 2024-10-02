0d3949a4cfcf8ac48d771fea9e924369f6fb9fbc
janisfs <janisfs@mail.ru>
2024-10-02 21:41:05 +0300

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,20 @@
-def main90:
-    pass
+from flask import Flaskform
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, EqualTo, ValidationError
+from app.models import User
 
+class RegistrationForm(Flaskform):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Регистрация')
 
-if __main__ ='__main__':
-    main()
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Такой пользователь уже существует. Пожалуйста, выберите другое имя пользователя.')
+
+class LoginForm(Flaskform):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Войти')
